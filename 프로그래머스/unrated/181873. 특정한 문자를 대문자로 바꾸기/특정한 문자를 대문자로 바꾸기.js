70ba48198a9f059360bba287eb86e86087418c68
huizhenz <133093192+huizhenz@users.noreply.github.com>
2023-10-24 03:24:41 +0900

    Delete 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기 directory

--- 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
+++
@@ -1,7 +0,0 @@
-const solution = (my_string, alp) => {
-    let newString = '';
-    for (let i = 0; i < my_string.length; i++) {
-        my_string[i] === alp ? newString += my_string[i].toUpperCase() : newString += my_string[i];
-    }
-    return newString;
-}
