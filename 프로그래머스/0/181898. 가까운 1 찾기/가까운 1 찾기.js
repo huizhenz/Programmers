39e05af76bf8ba7189bc439116e5aa166bb25517
huizhenz <133093192+huizhenz@users.noreply.github.com>
2024-02-27 17:39:39 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -0,0 +1,11 @@
+const solution = (arr, idx) => {
+  let answer = -1;
+
+  for (let i = idx; i < arr.length; i++) {
+    if (arr[i] === 1) {
+        answer = i;
+        break;
+    }
+  }
+  return answer;
+};
