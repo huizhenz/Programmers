a42240bc77bf9ea964d5dc8a91df2c88bf4d7c54
huizhenz <133093192+huizhenz@users.noreply.github.com>
2024-03-30 03:08:47 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.68 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -3,7 +3,6 @@ function solution(numer1, denom1, numer2, denom2) {
   let arr1 = [];
   let arr2 = [];
 
-  // 공약수 구하기
   for (let i = 1; i <= fractions[0]; i++) {
     if (fractions[0] % i === 0) arr1.push(i);
   }
@@ -11,13 +10,11 @@ function solution(numer1, denom1, numer2, denom2) {
     if (fractions[1] % i === 0) arr2.push(i);
   }
 
-  // 최대공약수 구하기
   const gcd = Math.max(
     ...arr1.filter((num) => {
       return arr2.includes(num);
     })
   );
 
-  // 최대공약수로 나눈 분자, 분모 값을 반환
   return [fractions[0] / gcd, fractions[1] / gcd];
 }
